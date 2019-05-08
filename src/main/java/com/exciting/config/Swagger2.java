bd8ebe56a7d558747c6dfbd00a6df8a23d459299
wujiaxing <jiaxing.wu@reach-core.com>
2019-05-08 18:40:17 +0800

    swagger

--- src/main/java/com/exciting/config/Swagger2.java
+++ src/main/java/com/exciting/config/Swagger2.java
@@ -18,7 +18,7 @@ public class Swagger2 {
 	public Docket createRestApi() {
 		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.exciting.web"))
+				.apis(RequestHandlerSelectors.basePackage("com.exciting.controller"))
 				.paths(PathSelectors.any()).build();
 	}
 
--- src/main/java/com/exciting/controller/HelloWorldController.java
+++ src/main/java/com/exciting/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.exciting.web;
+package com.exciting.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;

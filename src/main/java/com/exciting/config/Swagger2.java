6db9feee62b54bf400bac92524343b96ba9bdf01
wujiaxing <jiaxing.wu@reach-core.com>
2019-05-10 17:35:56 +0800

    Swagger优化

--- src/main/java/com/exciting/config/Swagger2.java
+++ src/main/java/com/exciting/config/Swagger2.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -27,7 +28,8 @@ public class Swagger2 {
 				.title("赛艇工具包")
 				.description("常用工具集合，让你亦可赛艇")
 				.termsOfServiceUrl("http://www.excting/")
-				.contact("Alastor").version("1.0").build();
+				.contact(new Contact("Alastor","",""))
+				.version("1.0").build();
 	}
 
 }

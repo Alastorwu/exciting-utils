136f1e814352092cb3e854f15e4f90d5f81cb10f
Alastor <jiaxing.wu@reach-core.com>
2019-07-24 17:39:28 +0800

    rsa

--- webapp/src/test/java/com/exciting/common/util/security/RSAUtilsTest.java
+++ webapp/src/test/java/com/exciting/common/util/security/RSAUtilsTest.java
@@ -46,24 +46,24 @@ public class RSAUtilsTest {
     public static void main(String[] args) throws Exception {
         String publicKey =
                 "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCL06hAZMhnXO/GhP5g05hAhdk4" +
-                        "PG0LZXChbqbdrMhPOaDkuLvvdmVVQHNBaVpPGNOBei6r7qvzQabpdsIiL4r5xjXo" +
-                        "QKG7wVVwYj5K8XkUxHVz1LUrpTIhaosIuPVb3duMGDsjwJM8u0dNuM09CJV8GWGU" +
-                        "dNSULA3dFIPwJGveuwIDAQAB";
+                "PG0LZXChbqbdrMhPOaDkuLvvdmVVQHNBaVpPGNOBei6r7qvzQabpdsIiL4r5xjXo" +
+                "QKG7wVVwYj5K8XkUxHVz1LUrpTIhaosIuPVb3duMGDsjwJM8u0dNuM09CJV8GWGU" +
+                "dNSULA3dFIPwJGveuwIDAQAB";
         String privateKey =
                 "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIvTqEBkyGdc78aE" +
-                        "/mDTmECF2Tg8bQtlcKFupt2syE85oOS4u+92ZVVAc0FpWk8Y04F6Lqvuq/NBpul2" +
-                        "wiIvivnGNehAobvBVXBiPkrxeRTEdXPUtSulMiFqiwi49Vvd24wYOyPAkzy7R024" +
-                        "zT0IlXwZYZR01JQsDd0Ug/Aka967AgMBAAECgYBSJNzyP4LwHkwKnzTl40jNlxR+" +
-                        "khkpmcVqQRwsnIXs7RkVZWWbDz4GsF9FbUQNtbMDnKjwfr1rogURoJ/5/14v8SJ0" +
-                        "2z25w7R6m87fMpZKQ9AvI/6FIC4Gvc0TtvZ9ft5toiGUqoBC2/yJJH4V6XHtj1Qb" +
-                        "18h0/z6D06jVaTQCAQJBANCeBs4hqqfXBiSl3kdD0RJrTEZvgv/+2DnnpiJ1x9BR" +
-                        "kEFLf1SW0XRCFzmPv+ycSyRt7hBJzmTLZqy9E45JUQECQQCrldUwLW/7AXYpIfgl" +
-                        "9ZYT/gvyejuna470Tgaz34DC4DoV4egLHVfoHuQiK42dm1nyg4KskGEZNseJmMaX" +
-                        "OrO7AkBwxjyfigubF942SYRTuhF3h5GAMh/7C2UQSG3DCzCtKKp24b0/mRg5ZNDx" +
-                        "SQhTfFoSo2qiW4O7cUlb8Ap2TKQBAkAF1U4uGR1Zdy9BLrp07+huPZDCaY1ln654" +
-                        "d76altqVxXG0FL4CwrieV46B6uEhnopsSDRFEZN7e/VEpED8Gy7JAkEAgC2eS9wt" +
-                        "sCbpW+guy7F6eiAYlZXipvyWcX+e+zE2JLHdR2qSoszs8Sx5bthtU7E46tclKK+v" +
-                        "GPTSay8VFUPZZQ==";
+                "/mDTmECF2Tg8bQtlcKFupt2syE85oOS4u+92ZVVAc0FpWk8Y04F6Lqvuq/NBpul2" +
+                "wiIvivnGNehAobvBVXBiPkrxeRTEdXPUtSulMiFqiwi49Vvd24wYOyPAkzy7R024" +
+                "zT0IlXwZYZR01JQsDd0Ug/Aka967AgMBAAECgYBSJNzyP4LwHkwKnzTl40jNlxR+" +
+                "khkpmcVqQRwsnIXs7RkVZWWbDz4GsF9FbUQNtbMDnKjwfr1rogURoJ/5/14v8SJ0" +
+                "2z25w7R6m87fMpZKQ9AvI/6FIC4Gvc0TtvZ9ft5toiGUqoBC2/yJJH4V6XHtj1Qb" +
+                "18h0/z6D06jVaTQCAQJBANCeBs4hqqfXBiSl3kdD0RJrTEZvgv/+2DnnpiJ1x9BR" +
+                "kEFLf1SW0XRCFzmPv+ycSyRt7hBJzmTLZqy9E45JUQECQQCrldUwLW/7AXYpIfgl" +
+                "9ZYT/gvyejuna470Tgaz34DC4DoV4egLHVfoHuQiK42dm1nyg4KskGEZNseJmMaX" +
+                "OrO7AkBwxjyfigubF942SYRTuhF3h5GAMh/7C2UQSG3DCzCtKKp24b0/mRg5ZNDx" +
+                "SQhTfFoSo2qiW4O7cUlb8Ap2TKQBAkAF1U4uGR1Zdy9BLrp07+huPZDCaY1ln654" +
+                "d76altqVxXG0FL4CwrieV46B6uEhnopsSDRFEZN7e/VEpED8Gy7JAkEAgC2eS9wt" +
+                "sCbpW+guy7F6eiAYlZXipvyWcX+e+zE2JLHdR2qSoszs8Sx5bthtU7E46tclKK+v" +
+                "GPTSay8VFUPZZQ==";
         System.err.println("私钥加密——公钥解密");
         String source = "这是一行测试RSA数字签名的无意义文字";
         System.out.println("原文字：\r\n" + source);

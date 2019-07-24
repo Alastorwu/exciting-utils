c9fe5c1422bcf5404a914f709ba5c475313da5e7
Alastor <jiaxing.wu@reach-core.com>
2019-07-24 16:19:02 +0800

    rsa

--- common/src/main/java/com/exciting/common/util/security/RSAUtils.java
+++ common/src/main/java/com/exciting/common/util/security/RSAUtils.java
@@ -336,16 +336,49 @@ public class RSAUtils {
         System.out.println("原文字：\r\n" + source);
         byte[] data = source.getBytes();
         byte[] encodedData = RSAUtils.encryptByPrivateKey(data, privateKey);
-        System.out.println("加密后：\r\n" + new String(encodedData));
-        byte[] decodedData = RSAUtils.decryptByPublicKey(encodedData, publicKey);
+        String encode = Base64Utils.encode(encodedData);
+        System.out.println("加密后：\r\n" + encode);
+        byte[] decodedData = RSAUtils.decryptByPublicKey(Base64Utils.decode(encode), publicKey);
         String target = new String(decodedData);
         System.out.println("解密后: \r\n" + target);
         System.err.println("私钥签名——公钥验证签名");
-        String sign = RSAUtils.sign(encodedData, privateKey);
+        String sign = RSAUtils.sign(Base64Utils.decode(encode), privateKey);
         System.err.println("签名:\r" + sign);
-        boolean status = RSAUtils.verify(encodedData, publicKey, sign);
+        boolean status = RSAUtils.verify(Base64Utils.decode(encode), publicKey, sign);
         System.err.println("验证结果:\r" + status);
     }*/
 
+    public static void main(String[] args) throws Exception {
+        String publicKey =
+                "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCL06hAZMhnXO/GhP5g05hAhdk4" +
+                "PG0LZXChbqbdrMhPOaDkuLvvdmVVQHNBaVpPGNOBei6r7qvzQabpdsIiL4r5xjXo" +
+                "QKG7wVVwYj5K8XkUxHVz1LUrpTIhaosIuPVb3duMGDsjwJM8u0dNuM09CJV8GWGU" +
+                "dNSULA3dFIPwJGveuwIDAQAB";
+        String privateKey =
+                "MIICdgIBADANBgkqhkiG9w0BAQEFAASCAmAwggJcAgEAAoGBAIvTqEBkyGdc78aE" +
+                "/mDTmECF2Tg8bQtlcKFupt2syE85oOS4u+92ZVVAc0FpWk8Y04F6Lqvuq/NBpul2" +
+                "wiIvivnGNehAobvBVXBiPkrxeRTEdXPUtSulMiFqiwi49Vvd24wYOyPAkzy7R024" +
+                "zT0IlXwZYZR01JQsDd0Ug/Aka967AgMBAAECgYBSJNzyP4LwHkwKnzTl40jNlxR+" +
+                "khkpmcVqQRwsnIXs7RkVZWWbDz4GsF9FbUQNtbMDnKjwfr1rogURoJ/5/14v8SJ0" +
+                "2z25w7R6m87fMpZKQ9AvI/6FIC4Gvc0TtvZ9ft5toiGUqoBC2/yJJH4V6XHtj1Qb" +
+                "18h0/z6D06jVaTQCAQJBANCeBs4hqqfXBiSl3kdD0RJrTEZvgv/+2DnnpiJ1x9BR" +
+                "kEFLf1SW0XRCFzmPv+ycSyRt7hBJzmTLZqy9E45JUQECQQCrldUwLW/7AXYpIfgl" +
+                "9ZYT/gvyejuna470Tgaz34DC4DoV4egLHVfoHuQiK42dm1nyg4KskGEZNseJmMaX" +
+                "OrO7AkBwxjyfigubF942SYRTuhF3h5GAMh/7C2UQSG3DCzCtKKp24b0/mRg5ZNDx" +
+                "SQhTfFoSo2qiW4O7cUlb8Ap2TKQBAkAF1U4uGR1Zdy9BLrp07+huPZDCaY1ln654" +
+                "d76altqVxXG0FL4CwrieV46B6uEhnopsSDRFEZN7e/VEpED8Gy7JAkEAgC2eS9wt" +
+                "sCbpW+guy7F6eiAYlZXipvyWcX+e+zE2JLHdR2qSoszs8Sx5bthtU7E46tclKK+v" +
+                "GPTSay8VFUPZZQ==";
+        System.err.println("公钥加密——私钥解密");
+        String source = "这是一行没有任何意义的文字，你看完了等于没看，不是吗？";
+        System.out.println("\r加密前文字：\r\n" + source);
+        byte[] data = source.getBytes();
+        byte[] encodedData = RSAUtils.encryptByPublicKey(data, publicKey);
+        String encode = Base64Utils.encode(encodedData);
+        System.out.println("加密后文字：\r\n" + encode);
+        byte[] decodedData = RSAUtils.decryptByPrivateKey(Base64Utils.decode(encode), privateKey);
+        String target = new String(decodedData);
+        System.out.println("解密后文字: \r\n" + target);
+    }
 
 }
